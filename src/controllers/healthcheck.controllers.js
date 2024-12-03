0beff4162d175772be5667b524168e03289e57b4
Divya Prakash <divyaprakashara09@gmail.com>
2024-12-03 13:05:41 +0530

    something in registerUser

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,5 +1,14 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+/*
+// async function requestHandler(req, res) {
+//   return res
+//     .status(200)
+//     .json(new ApiResponse(200, "OK", "Health check passed"))
+// }
+
+// const healthcheck = asyncHandler(requestHandler)
+*/
 
 const healthcheck = asyncHandler(async (req, res) => {
   return res
